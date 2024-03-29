c107216c3704e018ad27d9e42f131ae957da8252
Jacob Butler <ponmander@gmail.com>
2024-03-29 13:46:08 -0500

    Progress

---
+++ src/battle/area/jam/actor/bandits_yellow_common.h
@@ -0,0 +1,10 @@
+#ifndef _BANDITS_YELLOW_COMMON_H_
+#define _BANDITS_YELLOW_COMMON_H_
+
+enum N(BattleCommon) {
+    ACTOR_YELLOW_BANDIT       = ACTOR_ENEMY0,
+    ACTOR_HAMMER_BRO          = ACTOR_ENEMY1,
+    ACTOR_GIANT_CHOMP         = ACTOR_ENEMY2,
+};
+
+#endif // _BANDITS_YELLOW_COMMON_H_
--- src/battle/area/jam/area.c
+++ src/battle/area/jam/area.c
@@ -6,6 +6,9 @@ extern ActorBlueprint A(goomba);
 extern ActorBlueprint A(koopa_troopa);
 extern ActorBlueprint A(piranha_plant);
 extern ActorBlueprint A(false_bowser);
+extern ActorBlueprint A(koopa_bandits_yellow);
+extern ActorBlueprint A(giant_chain_chomp);
+extern ActorBlueprint A(hammer_bro);
 
 extern Stage A(jam_00);
 
@@ -25,11 +28,18 @@ Formation A(Formation_03) = {
     ACTOR_BY_IDX(A(false_bowser), BTL_POS_GROUND_C, 10),
 };
 
+Formation A(Formation_04) = {
+    ACTOR_BY_IDX(A(koopa_bandits_yellow), BTL_POS_GROUND_C, 10),
+    ACTOR_BY_IDX(A(hammer_bro), BTL_POS_GROUND_D, 9),
+    ACTOR_BY_IDX(A(giant_chain_chomp), BTL_POS_GROUND_A, 8),
+};
+
 BattleList A(Formations) = {
     BATTLE(A(Formation_00), A(jam_00), "Goomba"),
     BATTLE(A(Formation_01), A(jam_00), "Koopa Troopa"),
     BATTLE(A(Formation_02), A(jam_00), "Piranha Plant"),
     BATTLE(A(Formation_03), A(jam_00), "False Bowser"),
+    BATTLE(A(Formation_04), A(jam_00), "Yellow Bandit Koopa, Hammer Bro, Giant Chain Chomp"),
     {},
 };
 
